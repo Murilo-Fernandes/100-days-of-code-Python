44967b4d92ae3263267ec9dfa1b6d7176cb0f79c
murilo-fernandes <murilofernandeszl.23@gmail.com>
2024-10-15 18:03:59 -0300

    Day one finished

--- Day1.py
+++ Day1.py
@@ -1,4 +1,7 @@
-city = input("What's the name of the city you grew up in? ")
-pet = input("What's the name of your childhood pet? ")
+# Esse projeto é para gerar um nome para uma banda 
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your childhood pet?\n")
 
 print(f"You band's name could be {city} {pet}!")
