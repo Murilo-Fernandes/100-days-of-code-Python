c73b17cc19123e6edcdfa2ca0311457fe829b5ee
murilo-fernandes <murilofernandeszl.23@gmail.com>
2024-10-15 17:44:22 -0300

    Projeto dia 1

---
+++ Day1.py
@@ -0,0 +1,6 @@
+print("What's the name of the city you grew up in?")
+city = input()
+print("What's the name of your childhood pet?")
+pet = input()
+
+print(f"You band's name could be {city} {pet}!")
