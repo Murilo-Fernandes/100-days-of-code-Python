bc2debac1298e1d3d97c2306b71da85453f5eb9d
murilo-fernandes <murilofernandeszl.23@gmail.com>
2024-11-03 22:10:54 -0300

    Day 11 updated

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -9,25 +9,39 @@ def deal_card():
     return card 
 
 def calculate_score(cards):
-    """Take a list of cards, and return the scored calculated from the cards"""
+    """Take a list of cards, and return the score calculated from the cards"""
     if sum(cards) == 21 and len(cards) == 2:
-        return 0 
+        return 21
     elif sum(cards) > 21 and 11 in cards:
         cards.remove(11)
         cards.append(1)
     return sum(cards) 
 
-user_cards = []
-computer_cards = []
-is_game_over = False
+def compare(u_score, c_score):
+    if u_score == c_score:
+        return "Draw"
+    elif c_score == 21:
+        return "Lose, opponent has a blackjack!"
+    elif u_score == 21:
+        return "Win with a Blackjack!"
+    elif u_score > 21:
+        return "You went over. You lose!"
+    elif c_score > 21:
+        return "Opponent went over. You win!"
+    elif u_score > c_score:
+        return "You win!"
+    else:
+        return "You lose!"
 
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+def blackjack():    
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
 
-choice = input("Do you want to play a blackjack game? Type 'y' or 'n': ").lower()
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
-if choice == "y":
     while not is_game_over: 
         user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
@@ -35,15 +49,13 @@ if choice == "y":
         print(f"Your cards: {user_cards}, current score: {user_score}")
         print(f"Computer's first card: {computer_cards[0]}\n")
 
-        if user_score == 0 or computer_score == 0 or user_score > 21:
+        if user_score == 21 or computer_score == 21 or user_score > 21:
             is_game_over = True
-
         else:
             another_card = input("Type 'y' to get another card or type 'p' to pass: ").lower()
 
             if another_card == "y":
                 user_cards.append(deal_card())
-
             else:
                 is_game_over = True
 
@@ -53,15 +65,12 @@ if choice == "y":
                 print(f"Computer cards are: {computer_cards}. Total score: {computer_score}.\n")
 
     print(f"Your final hand is: {user_cards}, total score: {user_score}")
-    print(f"Computers final hand is: {computer_cards}, total score: {computer_score}")
+    print(f"Computer's final hand is: {computer_cards}, total score: {computer_score}")
 
-    if user_score > 21:
-        print("You went over 21. You lose!")
-    elif computer_score > 21 or user_score > computer_score:
-        print("Congrats, you won!")
-    elif user_score < computer_score:
-        print("You lose buddy.")
-    else: 
-        print("I'm afraid thats a draw.")
-else:
-    print("I think you typed something wrong, see you next time.")
+    print(compare(u_score=user_score, c_score=computer_score))
+
+while input("\nWanna play a blackjack game? 'y' for yes or anything for no: ") == "y":
+    os.system('cls')
+    blackjack()
+else: 
+    print("See you next time.")
