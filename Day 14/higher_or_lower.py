a78ad9ca13749288e0bf19f38a3e16d4511aae94
murilo-fernandes <murilofernandeszl.23@gmail.com>
2025-05-13 22:39:07 -0300

    Day 14 Finished

---
+++ Day 14/higher_or_lower.py
@@ -0,0 +1,79 @@
+# This is a higher or lower game where the user has to guess wich football player has more goals 
+
+from players import data 
+import random 
+import os 
+
+def higher_or_lower():
+    """Higher or Lower game where the user guesses which player has more goals."""
+    
+    print("Welcome to the Higher or Lower game!")
+    print("Guess which player has more goals.")
+    print("Type 'A' or 'B' to make your choice.")
+    print("Let's play!\n")
+
+    player_a = random.choice(data)
+    player_b = random.choice(data)
+    while player_a == player_b:
+        player_b = random.choice(data)
+    score = 0
+    game = True
+
+    while game: 
+        print(f"Current score: {score}")
+        if player_a == player_b:
+            player_b = random.choice(data)
+
+        print(f"Compare A: {player_a['name']}, a {player_a['description']}")
+        print("VS")
+        print(f"Against B: {player_b['name']}, a {player_b['description']}")
+
+        choice = input("Who has more goals? Type 'A' or 'B': ").upper()
+
+        if choice == 'A':
+
+            if player_a['goals'] > player_b['goals']:
+                os.system('cls')
+                print(f"Correct! {player_a['name']} has {player_a['goals']} goals. {player_b['name']} has {player_b['goals']} goals.")
+                score += 1
+
+                if score > 0:
+                    player_a = player_b
+                    player_b = random.choice(data)
+                    while player_a == player_b:
+                        player_b = random.choice(data)
+                else:
+                    player_b = random.choice(data)
+
+            else:
+                os.system('cls')
+                print(f"Wrong! {player_b['name']} has {player_b['goals']} goals. {player_a['name']} has {player_a['goals']} goals.")
+                game = False
+
+        elif choice == 'B':
+            os.system('cls')
+            if player_b['goals'] > player_a['goals']:
+                print(f"Correct! {player_b['name']} has {player_b['goals']} goals. {player_a['name']} has {player_a['goals']} goals.")
+                player_a = player_b
+                player_b = random.choice(data)
+                score += 1
+            else:
+                os.system('cls')
+                print(f"Wrong! {player_b['name']} has {player_b['goals']}.")
+                game = False
+        else:
+            os.system('cls')
+            print("Invalid input. Please type 'A' or 'B'.")
+            continue
+
+    print("Game over. Your final score is:", score)
+    play_again = input("Do you want to play again? (y/n): ").lower()
+    if play_again != 'y':
+        game = False
+        print("Thanks for playing!")
+    else:
+        os.system('cls')
+        higher_or_lower()
+
+
+higher_or_lower()
---
+++ Day 14/players.py
@@ -0,0 +1,87 @@
+data = [
+    {
+        "name": "Lionel Messi",
+        "goals": 700,
+        "description": "Lionel Messi is an Argentine professional footballer."
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "goals": 800,
+        "description": "Cristiano Ronaldo is a Portuguese professional footballer.",
+    },
+    {
+        "name": "Neymar",
+        "goals": 500,
+        "description": "Neymar is a Brazilian professional footballer.",
+    },
+    {
+        "name": "Kylian Mbappe",
+        "goals": 300,
+        "description": "Kylian Mbappe is a French professional footballer.",
+    },
+    {
+        "name": "Robert Lewandowski",
+        "goals": 600,
+        "description": "Robert Lewandowski is a Polish professional footballer.",
+    },
+    {
+        "name": "Karim Benzema",
+        "goals": 400,
+        "description": "Karim Benzema is a French professional footballer.",
+    },
+    {
+        "name": "Sergio Ramos",
+        "goals": 200,
+        "description": "Sergio Ramos is a Spanish professional footballer.",
+    },
+    {
+        "name": "Gareth Bale",
+        "goals": 100,
+        "description": "Gareth Bale is a Welsh professional footballer.",
+    },
+    {
+        "name": "Zlatan Ibrahimovic",
+        "goals": 900,
+        "description": "Zlatan Ibrahimovic is a Swedish professional footballer.",
+    },
+    {
+        "name": "Andres Iniesta",
+        "goals": 50,
+        "description": "Andres Iniesta is a Spanish professional footballer.",
+    },
+    {
+        "name": "Xavi Hernandez",
+        "goals": 30,
+        "description": "Xavi Hernandez is a Spanish professional footballer.",
+    },
+    {
+        "name": "Ronaldinho",
+        "goals": 200,
+        "description": "Ronaldinho is a Brazilian professional footballer.",
+    },
+    {
+        "name": "Pele",
+        "goals": 1281,
+        "description": "Pele is a Brazilian professional footballer.",
+    },
+    {
+        "name": "Diego Maradona",
+        "goals": 198,
+        "description": "Diego Maradona is an Argentine professional footballer.",
+    },
+    {
+        "name": "George Best",
+        "goals": 231,
+        "description": "George Best is a Northern Irish professional footballer.",
+    },
+    {
+        "name": "David Beckham",
+        "goals": 127,
+        "description": "David Beckham is an English professional footballer.",
+    },
+    {
+        "name": "Mohamed Salah",
+        "goals": 343,
+        "description": "Mohamed Salah is an Egyptian professional footballer.",
+    }
+]
